56280962bdd97b358f902def9ffb24508a3a2412
Alin Ali Hassan <hassanalinali@gmail.com>
2023-11-11 00:02:53 +0100

    Brewfile now prefixed with dot when moved to ~

--- install.sh
+++ install.sh
@@ -54,11 +54,11 @@ if ! command -v brew &> /dev/null; then
 fi
 
 ## Symlink Brewfile
-ln -sf "$DOTFILES_PATH/Brewfile" ~/Brewfile
+ln -sf "$DOTFILES_PATH/Brewfile" ~/.Brewfile
 
 ## Update Homebrew and install packages from Brewfile
 brew update
-brew bundle --file=~/Brewfile
+brew bundle --file=~/.Brewfile
 
 # LunarVim
 if ! command -v lvim &> /dev/null; then

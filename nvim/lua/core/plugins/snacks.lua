b2fb9b9366c5ba5528097d02f554d5769464efb1
Alin Ali Hassan <hassanalinali@gmail.com>
2025-10-31 10:33:45 +0100

    Added cutlass.nvim

--- nvim/lua/core/plugins/snacks.lua
+++ nvim/lua/core/plugins/snacks.lua
@@ -18,7 +18,6 @@ return {
       preset = {
         pick = nil,
         keys = {
-          { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
           { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
           { icon = ' ', key = 't', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
           { icon = ' ', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },

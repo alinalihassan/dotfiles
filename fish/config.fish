d29e4a57a708e0856083b9dc55a1d65554f3eee3
Alin Ali Hassan <hassanalinali@gmail.com>
2025-12-26 16:30:55 +0200

    Moved OrbStack to path instead of config

--- fish/config.fish
+++ fish/config.fish
@@ -12,7 +12,3 @@ zoxide init --cmd cd fish | source
 # Configure fzf.fish keybindings (Ctrl+F then letter)
 # \cf = Ctrl+F, then the letter follows
 fzf_configure_bindings --directory=\cff --history=\cfh --git_status=\cfs --git_log=\cfl --processes=\cfp --variables=\cfv
-
-# Added by OrbStack: command-line tools and integration
-# This won't be added again if you remove it.
-source ~/.orbstack/shell/init2.fish 2>/dev/null || :
